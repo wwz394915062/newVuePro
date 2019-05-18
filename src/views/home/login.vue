7f8ec81758b2f849b67ba78303607bd5ffdd50f5
wwz394915062 <394915062@qq.com>
2019-05-18 10:15:56 +0800

    vue国际化分支

---
+++ src/views/home/login.vue
@@ -0,0 +1,22 @@
+<<template>
+  <div class="page">
+
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+
+    }
+  },
+  components: {
+
+  }
+}
+</script>
+
+<style scoped lang="">
+</style>
+
